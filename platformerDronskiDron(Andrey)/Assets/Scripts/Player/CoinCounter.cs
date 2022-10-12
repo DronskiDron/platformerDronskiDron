432429dd5eb2f5acd737f88543e34ebce46eeb91
DronskiDron <andronaf93@gmail.com>
2022-10-12 17:40:36 +0300

    Домашняя работа №4

---
+++ platformerDronskiDron(Andrey)/Assets/Scripts/Player/CoinCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CoinCounter : MonoBehaviour
+    {
+        private float _money;
+
+        public void GetMoney(float moneyFromObjects)
+        {
+            _money += moneyFromObjects;
+        }
+
+
+        public void MoneyConsoleWriter()
+        {
+            Debug.Log($"Поздравляю! У Вас {_money} монет!");
+        }
+    }
+}
